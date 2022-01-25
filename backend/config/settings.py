2f9feda3881fdfa9c85013dea912d4f11e46e5fa
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-25 17:40:17 -0500

    trying again to get the DATABASE to work

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -79,15 +79,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tilt',
-        'USER': 'tilt_admin',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tilt',
+#         'USER': 'tilt_admin',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#     }
+# }
 
 DATABASE = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
