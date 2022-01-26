a79a663ddb4296bbe787a235fadaa509126cd898
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-26 18:17:07 -0500

    adjustments to app.js routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,44 @@
 import './App.css';
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import Layout from './components/Layout/Layout.jsx';
-
+import Home from "./screens/Home/Home.jsx";
+import { verifyUser } from "./services/users";
 
 function App() {
+  const [user, setUser] = useStatue(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
-      <Layout />
-      <p>THIS IS MY APP</p>
+      <Routes>
+        <Route path='/' element={<Home user={user} />} />
+        <Route path='/signUp' element={null} />
+        <Route path='/signIn' element={null} />
+        <Route path='/signOut' element={null} />
+        <Route path='/venue' element={null} />
+        <Route path='/add-venue'
+          element={
+            user ? (
+              <VenueCreate user={user} />
+            ) : (
+              <Navigate to='/add-product' />
+            )
+          }
+        />
+        {/*end of add-venue */}
+        <Route
+          path='/venues/:id'
+          element={<VenueDetail user={user} />}
+        />
+      </Routes>
+
     </div>
   );
 }
--- frontend/src/components/Nav/Nav.jsx
+++ frontend/src/components/Nav/Nav.jsx
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <navbar>
+    <nav>
       <div className='nav-container'>
         <div className='nav-links'>
           <Link className="signIn" to='/signIn'>SignIn</Link>
           <Link className="signUp" to='/signUp'>SignUp</Link>
         </div>
       </div>
-    </navbar>
+    </nav>
   );
 };
 
