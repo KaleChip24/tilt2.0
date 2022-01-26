50346ae278bd4b9f83ed0d7378a7d7cea2829e58
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-26 11:51:17 -0500

    settings update comment out static files if/else statement. .ds_store add

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -135,15 +135,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-if IS_PRODUCTION:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-  STATIC_URL = '/static/'
-  STATICFILES_DIRS = (
+# if IS_PRODUCTION:
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
       os.path.join(BASE_DIR, 'static'),
-  )
+)
   
-else:
-  STATIC_URL = '/static/'
+# else:
+#   STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
