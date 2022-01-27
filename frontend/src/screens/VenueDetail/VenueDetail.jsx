243075b4dc92f30716256754dbbd042c24a5f8d0
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-27 14:41:59 -0500

    api id congif changes

--- frontend/src/screens/VenueDetail/VenueDetail.jsx
+++ frontend/src/screens/VenueDetail/VenueDetail.jsx
@@ -1,21 +1,23 @@
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
-import { getVenue, getVenues } from "../../services/venueApi";
-import { useParmas, Link, Navigate, useParams } from "react-router-dom";
+import { getVenues } from "../../services/venueApi";
+import { useParams } from "react-router-dom";
 
-const VenueDetail = () => {
-  const [veune, setVenue] = useState(null);
+
+const VenueDetail = (props) => {
+  const [venue, setVenue] = useState(null);
   const [isLoaded, setLoaded] = useState(false);
-  const { index } = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     const fetchVenues = async () => {
-      const veune = await getVenues(index);
-      setVenue(veune);
+      const venue = await getVenues(id);
+      console.log(venue)
+      setVenue(venue);
       setLoaded(true);
     };
     fetchVenues()
-  }, [index]);
+  }, [id]);
 
   if (!isLoaded) {
     return <h1>You Tilted....Waiting for ball to drop</h1>
@@ -25,7 +27,9 @@ const VenueDetail = () => {
   return (
     <Layout>
       <div className="venue-detail">
-
+        <h4>{props.name}</h4>
+        <p>{props.address}</p>
+        <p>{props.city}{props.state}</p>
       </div>
     </Layout>
   );
--- frontend/src/services/venueApi.jsx
+++ frontend/src/services/venueApi.jsx
@@ -3,7 +3,6 @@ import api from "./apiConfig";
 export const getVenues = async () => {
   try {
     const response = await api.get("/venues/");
-    console.log(response.data)
     return response.data;
 
   } catch (error) {
