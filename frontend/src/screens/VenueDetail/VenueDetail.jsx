d75a0ea76eefa2519018b77a53ac2ab92b7c61cd
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-28 12:37:53 -0500

    WIP VenueDetail.css

--- frontend/src/screens/VenueDetail/VenueDetail.jsx
+++ frontend/src/screens/VenueDetail/VenueDetail.jsx
@@ -1,15 +1,14 @@
 import { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import { getVenue } from "../../services/venueApi";
-import { getMachines } from "../../services/machineApi";
 import { useParams } from "react-router-dom";
-// import Machines from "../../components/Machines/Machines"
+import "./VenueDetail.css"
+
 
 
 
 const VenueDetail = (props) => {
   const [venue, setVenue] = useState([]);
-  const [machines, setMachines] = useState([]);
   const [isLoaded, setLoaded] = useState(false);
   const { id } = useParams();
 
@@ -22,34 +21,32 @@ const VenueDetail = (props) => {
     fetchVenue()
   }, [id]);
 
-  useEffect(() => {
-    const fetchMachines = async () => {
-      const allMachines = await getMachines();
-      console.log(allMachines)
-      setMachines(allMachines);
-    }
-    fetchMachines()
-  }, [])
 
   if (!isLoaded) {
     return <h1>You Tilted....Waiting for ball to drop</h1>
   }
 
+  // const [name, address, city, state, machine]
+
 
   return (
     <Layout>
       <div className="venue-detail">
-        <h4>{venue.name}</h4>
-        <p>{venue.address}</p>
-        <p>{venue.city} {venue.state}</p>
-        <div>{venue.machine}</div>
-        {/* <div>
-          {machines.map((venue.name == machine.venue) => {
-          return(
-          <div>{machine.name}</div>
-          )
-          })}
-        </div> */}
+        <h1 className="venue-title">{venue.name}</h1>
+        <p className="venue-address">{venue.address}</p>
+        <p className="venue-city-state">{venue.city} {venue.state}</p>
+      </div>
+      <h1 className="machines-block">machines</h1>
+      <div className="machines">
+        {venue.machine.map((machine, index) => {
+          return (
+            <div className="machine-cards">
+              <h3>{machine.name}</h3>
+              <p>{machine.price}</p>
+              <p>{machine.comments}</p>
+            </div>
+          );
+        })}
       </div>
     </Layout>
   );
