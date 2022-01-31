b160ab60315b5b802dc19265f5ea09292f20348f
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-31 12:41:15 -0500

    updated settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -204,7 +204,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=3),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
