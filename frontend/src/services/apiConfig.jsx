96e36c3912a773f4a89e9eb5ef6b30f9e96ea8a5
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-31 12:09:31 -0500

    edit apiConfig

--- frontend/src/services/apiConfig.jsx
+++ frontend/src/services/apiConfig.jsx
@@ -9,7 +9,7 @@ const getToken = () => {
 const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://tilt2.herokuapp.com/"
+      ? "https://tilt2.herokuapp.com/"
       : "http://localhost:8000/",
 });
 
