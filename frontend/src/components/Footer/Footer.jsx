e7c5b292566c0112a4c58ebd36ba6a7744fba7e8
KaleOHara <92695012+KaleChip24@users.noreply.github.com>
2022-01-31 01:09:14 -0500

    footer and footer.css

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,10 +1,34 @@
+import "./Footer.css"
+import { VscGithub } from 'react-icons/vsc'
+import { FiLinkedin } from 'react-icons/fi'
 
-const footer = () => {
+function Footer(props) {
   return (
-    <div>
-      <footer></footer>
-    </div>
-  );
-};
+    <footer className="foot-container">
+      <div className="links">
+        <a
+          className="git-link"
+          href="https://github.com/KaleChip24"
+          target="_blank"
+          rel="noreferrer">
+          <strong>
+            <VscGithub></VscGithub>
+          </strong>
+        </a>
+        <a
+          className="linked-link"
+          href="https://www.linkedin.com/in/oharakale/"
+          target="_blank"
+          rel="noreferrer">
+          <FiLinkedin></FiLinkedin>
+        </a>
+      </div>
+      <div className="copyright-yr">
+        <small>&copy;2022</small>
+      </div>
 
-export default footer;
+    </footer>
+  );
+}
+
+export default Footer;
