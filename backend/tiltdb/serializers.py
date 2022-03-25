5590efa9390f7107819d33c6928f2b22d68aba48
KaleChip24 <oharakale@gmail.com>
2022-03-25 10:47:00 -0400

    rerun

--- backend/tiltdb/serializers.py
+++ backend/tiltdb/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers, fields
 from .models import Venue, Machine
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
     
 
 class MachineSerializer(serializers.ModelSerializer):
@@ -38,4 +41,25 @@ class VenueSerializer(serializers.ModelSerializer):
         machine.comments = machine_data.get('comments', machine.comments)
         machine.save()
     return instance
-        
+
+class UserSerializer(serializers.ModelSerializer):
+  username = serializers.CharField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
+  password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+  password2 = serializers.CharField(write_only=True, required=True)
+  
+  class Meta:
+    model = User
+    fields = ('username', 'password', 'password2')
+  
+  def validate(self, attrs):
+    if attrs['password'] !=attrs['password2']:
+      raise serializers.ValidationError({"password": "Password fields do not match."})
+    return attrs
+  
+  def crate(self, validated_data):
+    user = User.objects.create(
+      username=validated_data['username'],
+    )
+    user.set_password(validated_data['password'])
+    user.save()
+    return user
--- backend/tiltdb/views.py
+++ backend/tiltdb/views.py
@@ -1,5 +1,8 @@
-from rest_framework import viewsets, permissions
-from .serializers import VenueSerializer, MachineSerializer
+from rest_framework import viewsets, generics
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly 
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from .serializers import VenueSerializer, MachineSerializer, UserSerializer
 from .models import Venue, Machine
 
 
@@ -7,11 +10,14 @@ from .models import Venue, Machine
 class VenueViewSet(viewsets.ModelViewSet):
   queryset = Venue.objects.all()
   serializer_class = VenueSerializer
-  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+  permission_classes = (IsAuthenticatedOrReadOnly,)
   
 class MachineViewSet(viewsets.ModelViewSet):
   queryset = Machine.objects.all()
   serializer_class = MachineSerializer
-  permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+  permission_classes = (IsAuthenticatedOrReadOnly,)
   
-  
+class UserCreate(generics.CreateAPIView):
+  queryset = User.objests.all()
+  serializer_class = UserSerializer
+  permission_class = (AllowAny)
